27edd4e68adc3efd2ef89f9dc6119e9a2ca43004
xskxzr <xskxzr@gmail.com>
2017-05-26 01:42:36 +0800

    实现不展示的方法MA79、GS83、BE84

--- SimplePolygonConvexHull/GhoshShyamasundar83.cpp
+++ SimplePolygonConvexHull/GhoshShyamasundar83.cpp
@@ -1,9 +1,40 @@
 #include "GhoshShyamasundar83.h"
 
+// 从max到min构造一半的凸包
+void GhoshShyamasundar83::getHalfConvexHull(SimplePolygon &sp, int max, int min)
+{
+	sp.convexHull.push_back(max);
+	auto size = sp.points.size();
+	int i = (max + 1) % size;
+
+	if (i == min) return;
+	sp.convexHull.push_back(i);
+
+	for (i = (i + 1) % size; i != min; i = (i + 1) % size) {
+		while (sp.convexHull.size() >= 2 &&
+		       !toLeft(sp.points[sp.convexHull[sp.convexHull.size() - 2]],
+		               sp.points[sp.convexHull.back()],
+		               sp.points[i]))
+			sp.convexHull.pop_back();
+
+		if ((sp.points[min].x < sp.points[max].x) ==
+		    (sp.points[i].x < sp.points[sp.convexHull.back()].x))
+			sp.convexHull.push_back(i);
+	}
+
+	while (sp.convexHull.size() >= 2 &&
+	       !toLeft(sp.points[sp.convexHull[sp.convexHull.size() - 2]],
+	               sp.points[sp.convexHull.back()],
+	               sp.points[i]))
+		sp.convexHull.pop_back();
+}
 
 void GhoshShyamasundar83::getConvexHull(SimplePolygon & sp)
 {
-
+	sp.convexHull.clear();
+	auto min = sp.getLeftMostThenLowestPoint(), max = sp.getRightMostThenHighestPoint();
+	getHalfConvexHull(sp, max, min);
+	getHalfConvexHull(sp, min, max);
 }
 
 void GhoshShyamasundar83::getConvexHullForDisplay(SimplePolygon & sp, Displays & displays)
