e48e3410d0f71f4413fe6dd9fa24d483529d3562
zyeric <619828575@qq.com>
2017-05-27 21:15:00 +0800

    Melkman97算法与显示实现

--- SimplePolygonConvexHull/Melkman87.h
+++ SimplePolygonConvexHull/Melkman87.h
@@ -8,4 +8,6 @@ class Melkman87 : Algorithm
 public:
 	void getConvexHull(SimplePolygon & sp);
 	void getConvexHullForDisplay(SimplePolygon & sp, Displays & displays);
+private:
+	Display createDisplay(SimplePolygon& sp, deque<int>& Q);
 };
